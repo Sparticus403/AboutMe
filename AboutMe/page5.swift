c61df5b0a1d8328f4b0e551071a2ba7320b794c3
Sparticus403 <twil8980@740s-pl-332.local>
2016-10-05 07:52:26 -0600

    finished basic setup for about me app

---
+++ AboutMe/page5.swift
@@ -0,0 +1,17 @@
+//
+//  page5.swift
+//  AboutMe
+//
+//  Created by Williamson, Todd on 10/5/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class page5 : UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
